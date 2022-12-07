b860daf2856d38293606d003480dbc404474df6f
VincenzoDiMasi <v.dimasi93@gmail.com>
2022-12-07 16:16:32 +0100

    Add for

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log ('JS OK')
+
+/*Scrivi un programma che stampi in console i numeri da 1 a 100, ma che
+per i multipli di 3 stampi “Fizz” al posto del numero
+per i multipli di 5 stampi “Buzz” al posto del numero.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.*/
+
+//Prendo gli elementi dal DOM
+const targetElemenent = document.getElementsByClassName ('target')
+console.log (targetElemenent)
+
+//Faccio un ciclo for di numeri da 1 a 100
+for (let i = 1; i <= 100; i++)
+console.log (i)
